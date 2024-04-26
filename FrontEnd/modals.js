bc3802d9ae11b67a5dbe253ce878e4e6c35bd70b
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2024-04-26 15:09:43 +0200

    mettre des options dans le modals.js

--- FrontEnd/modals.js
+++ FrontEnd/modals.js
@@ -141,7 +141,7 @@ async function afficherTravauxDansGalerie() {
 afficherTravauxDansGalerie();
 
 
-
+/* *****    formulaire pour ajout de project  ****** */
 //ajout photo de mon repertoire
 let imageURL = "";
 function uploadImage() {
@@ -195,6 +195,82 @@ window.addEventListener("load", function() {
     uploadImage()
 });
 
+async function ajoutOption(){
+    try {
+        // Appeler la fonction pour récupérer toutes les catégories
+        const reponse = await fetch("http://localhost:5678/api/categories");
+        const categories = await reponse.json();
+
+        // Sélectionnez le menu déroulant des catégories
+        const categorieSelect = document.getElementById("modal-categorie");
+
+        // Ajouter l'option vide comme première option
+        const emptyOption = document.createElement("option");
+        emptyOption.id = "modal-option";
+        emptyOption.value = ""; // Valeur vide
+        emptyOption.textContent = ""; // Texte vide
+        categorieSelect.appendChild(emptyOption);
+
+        // Boucle à travers les catégories et créez les options
+        categories.forEach(categorie => {
+            // Créez une nouvelle option
+            const option = document.createElement("option");
+            option.id = "modal-option";
+            
+            // Définissez la valeur et le texte de l'option
+            option.value = categorie.name;
+            option.innerText = categorie.name;
+            
+            // Ajoutez l'option au menu déroulant
+            categorieSelect.appendChild(option);
+
+        });
+    } catch (error) {
+        console.error("Une erreur s'est produite lors de l'ajout des options de catégorie :", error);
+    }
+}
+// Appeler la fonction pour ajouter les options au chargement de la page
+ajoutOption(); 
+
+
+
+
+
+
+// Récupérer les éléments nécessaires
+const validerButton = document.getElementById("valider");
+// Ajoutez un écouteur d'événements pour détecter les changements de valeur
+const titleInput = document.getElementById("title");
+titleInput.addEventListener("input", function() {
+    // Récupérez la valeur saisie dans l'élément input
+    const saisie = titleInput.value; 
+    console.log(saisie);
+    localStorage.setItem("titleSaisir", saisie);
+    verifierRemplissage()
+});
+const contenuOption = document.getElementById("modal-categorie")
+contenuOption.addEventListener("change", function(event){
+    const option = contenuOption.value
+    console.log(contenuOption.value)
+    localStorage.setItem("option", option);
+    verifierRemplissage()
+});
+
+//vérifier que le contenu de la modal ajouter n'est pas vide pour activer le bouton
+function verifierRemplissage() {
+    const titreSaisi = titleInput.value.trim(); // Récupérer la valeur saisie dans le titre
+    const option = contenuOption.value; // Récupérer la catégorie sélectionnée
+
+
+    // Vérifier si l'image est déjà chargée et si le titre et la catégorie ne sont pas vides
+    if ( localStorage.getItem("imageURL") && titreSaisi !== "" && option !== "") {
+        // Si tous les champs sont remplis, activer le bouton de validation
+        validerButton.style.backgroundColor = "#1D6154";
+    } else {
+        // Sinon, désactiver le bouton de validation
+        validerButton.style.backgroundColor = "";
+    }
+}
 
 
 
