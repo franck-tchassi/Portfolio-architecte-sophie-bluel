a6d382b56427a91d6388fba6e76026167aba70da
franck-tchassi <115140772+franck-tchassi@users.noreply.github.com>
2024-04-29 13:09:56 +0200

    Ajout de la fonctionnalité d'ajout de travaux dans le fichier modals.js

--- FrontEnd/modals.js
+++ FrontEnd/modals.js
@@ -1,11 +1,37 @@
+//se logout pour se déconnecter
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", function(e){
+    e.preventDefault();
+	window.location.href="login.html";
+	localStorage.removeItem("authToken")
+})
+
+// Exécuter le code pour masquer l'élément avec l'ID "filters" si la redirection vers index.html a été effectuée
+document.addEventListener("DOMContentLoaded", function() {
+    const authToken = localStorage.getItem('authToken');
+    if (authToken !== null) {
+        document.getElementById("filters").style.display = "none";
+		document.getElementById("login").style.display = "none";
+		document.getElementById("logout").style.display = "flex";
+		document.querySelector(".modifier").style.display = "flex";
+        document.querySelector(".adminfeature-edition").style.display = "flex";
+    }
+});
+
 // ouvrir la modal suprimer lorsque l'on click sur le bouton modifier
 const btnModifier = document.querySelector("#icone-modifier");
+const modeEdition = document.getElementById("banner-modifier")
 btnModifier.addEventListener("click", function(e){
     e.preventDefault();
     const modalOuvert = document.getElementById("deleteView").style.display="flex";
     (modalOuvert == "flex") ? document.querySelector("body").style.backgroundColor="#0000004D" : "" ;
-    
 })
+modeEdition.addEventListener("click", function(e){
+    e.preventDefault();
+    const modalOuvert = document.getElementById("deleteView").style.display="flex";
+    (modalOuvert == "flex") ? document.querySelector("body").style.backgroundColor="#0000004D" : "" ;
+})
+
 // ouvrir la modal ajouter element lorsque l'on click sur le bouton du modal supprimer
 const btnModal1 = document.querySelector("#add-picture");
 btnModal1.addEventListener("click", function(e){
@@ -14,12 +40,12 @@ btnModal1.addEventListener("click", function(e){
     (modalOpen == "flex") ? document.querySelector("body").style.backgroundColor="#0000004D" : "" ;
     (modalOpen == "flex") ? document.getElementById("deleteView").style.display="none" : "" ;
 })
+
 // clicker pour faire une marche en arrière sur la modal supprimer
 const btnModal2 = document.querySelector(".return-view");
 btnModal2.addEventListener("click", function(e){
     e.preventDefault();
     const modalReturn = document.getElementById("deleteView").style.display="flex";
-   // (modalOpen == "flex") ? document.querySelector("body").style.backgroundColor="#0000004D" : "" ;
     (modalReturn == "flex") ? document.getElementById("addView").style.display="none" : "" ;
 
     //Réinitialisation de l'affichage de l'image et suppression de l'URL de l'image du localStorage lorsque on click sur le bouton revenir en arrière
@@ -31,12 +57,11 @@ btnModal2.addEventListener("click", function(e){
     
     //vider le title et option lorsque l'on quitte la modal ajouter
     titleInput.value = "";
-    localStorage.removeItem("titleSaisir")
     contenuOption.value = "";
-    localStorage.removeItem("option")
     //désactiver le bouton de validation
-    validerButton.style.backgroundColor = "";
+    verifierRemplissage();
 })
+
 // Sélectionnez le bouton de fermeture de la modal supprimer
 const deleteCloseButton = document.querySelector(".close-view1");
 deleteCloseButton.addEventListener("click", function(e) {
@@ -65,14 +90,10 @@ addCloseButton.addEventListener("click", function(e) {
 
     //vider le title et option lorsque l'on quitte la modal ajouter
     titleInput.value = "";
-    localStorage.removeItem("titleSaisir")
     contenuOption.value = "";
-    localStorage.removeItem("option")
     //désactiver le bouton de validation
-    validerButton.style.backgroundColor = "";
+    verifierRemplissage();
 });
-
-
 
 // Fonction pour afficher les travaux dans la galerie de photos à supprimer
 async function afficherTravauxDansGalerie() {
@@ -133,17 +154,13 @@ async function afficherTravauxDansGalerie() {
                 console.log("Travail non trouvé dans la liste.");
             }
         });
-       
     });
-   
 }
 // Appel de la fonction pour afficher les travaux dans la galerie
 afficherTravauxDansGalerie();
 
-
 /* *****    formulaire pour ajout de project  ****** */
 //ajout photo de mon repertoire
-let imageURL = "";
 function uploadImage() {
     const fileInput = document.getElementById("file-upload");
     const uploadedImage = document.getElementById("uploaded-image");
@@ -163,9 +180,8 @@ function uploadImage() {
 
                     reader.onload = function(e) {
                         
-                        imageURL = e.target.result;
-                        localStorage.setItem("imageURL", imageURL);
-                        uploadedImage.src = imageURL;
+                         
+                        uploadedImage.src = e.target.result;
                         uploadedImage.style.display = "block"; // Afficher l'image
                         document.querySelector(".custom-file-upload").style.display = "none";
                         document.querySelector(".custom p").style.display = "none";
@@ -187,14 +203,12 @@ function uploadImage() {
                 uploadedImage.src = ""; // Réinitialiser l'image
                 uploadedImage.style.display = "none"; // Cacher l'image
             }
-            
         }
     });
 }
-window.addEventListener("load", function() {
-    uploadImage()
-});
+uploadImage()
 
+//function qui ajoute des options dans la modal ajouter
 async function ajoutOption(){
     try {
         // Appeler la fonction pour récupérer toutes les catégories
@@ -218,8 +232,10 @@ async function ajoutOption(){
             option.id = "modal-option";
             
             // Définissez la valeur et le texte de l'option
-            option.value = categorie.name;
+            option.value = categorie.id;
             option.innerText = categorie.name;
+
+           
             
             // Ajoutez l'option au menu déroulant
             categorieSelect.appendChild(option);
@@ -233,12 +249,8 @@ async function ajoutOption(){
 ajoutOption(); 
 
 
-
-
-
-
 // Récupérer les éléments nécessaires
-const validerButton = document.getElementById("valider");
+const validerButton = document.getElementById("modal-valider");
 // Ajoutez un écouteur d'événements pour détecter les changements de valeur
 const titleInput = document.getElementById("title");
 titleInput.addEventListener("input", function() {
@@ -258,20 +270,67 @@ contenuOption.addEventListener("change", function(event){
 
 //vérifier que le contenu de la modal ajouter n'est pas vide pour activer le bouton
 function verifierRemplissage() {
-    const titreSaisi = titleInput.value.trim(); // Récupérer la valeur saisie dans le titre
-    const option = contenuOption.value; // Récupérer la catégorie sélectionnée
-
-
+   // const title = titleInput.value.trim(); // Récupérer la valeur saisie dans le titre
+   // const category = contenuOption.value; // Récupérer la catégorie sélectionnée
+    const fileInput = document.getElementById("file-upload");
+    const title = document.getElementById("title");
+    const category = document.getElementById("modal-categorie");
+    
+    
     // Vérifier si l'image est déjà chargée et si le titre et la catégorie ne sont pas vides
-    if ( localStorage.getItem("imageURL") && titreSaisi !== "" && option !== "") {
-        // Si tous les champs sont remplis, activer le bouton de validation
-        validerButton.style.backgroundColor = "#1D6154";
-    } else {
+    if ( fileInput.files.length > 0 && title.value !== "" && category.value !== ""){
+      // Si tous les champs sont remplis, activer le bouton de validation
+      validerButton.style.backgroundColor = "#1D6154";
+      validerButton.style.cursor = "pointer";
+    }
+    else {
         // Sinon, désactiver le bouton de validation
         validerButton.style.backgroundColor = "";
     }
 }
+verifierRemplissage()
 
+//événement qui permet d'ajouter des travaux
+document.querySelector('#modal-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    
+    const fileInput = document.getElementById("file-upload");
+    const title = document.getElementById("title").value;
+    const category = document.getElementById("modal-categorie").value;
+   
+
+    const formData = new FormData();
+    const fileData = fileInput.files[0];
+    
+    formData.append("image", fileData, fileData.name);
+    formData.append("title", title);
+    formData.append("category", category);
+    
+    const authToken = localStorage.getItem("authToken");
+
+    try {
+        const response = await fetch(`http://localhost:5678/api/works/`, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${authToken}`,
+                'Accept': 'application/json'
+            },
+            body: formData
+        });
+
+        if (response.ok) {
+            alert('Mise à jour réussie');
+            // Afficher les travaux dans la galerie après l'ajout
+            // afficherTravauxDansGalerie()
+        } else {
+            alert('Les modifications ne seront pas sauvegardées');
+        }
+    } catch (error) {
+        console.error("Une erreur s'est produite lors de l'envoi de la requête :", error);
+    }
+    
+    afficherTravauxDansGalerie();
+})
 
 
 
